b49cc4220d7023453ae3cc2606437525c7454273
YZ-YI <yiyuzhe0125@outlook.com>
2020-08-18 00:53:00 -0600

    slope movement bug still needs fixing

--- 2DPixelGame/Assets/Scripts/PlayerController.cs
+++ 2DPixelGame/Assets/Scripts/PlayerController.cs
@@ -8,13 +8,25 @@ public class PlayerController : MonoBehaviour
     public float dashSpeed = 20f;
     public bool doubleJumpEnabled = false;
     public float crouchSpeed = 7f;                      //player cannot dash while crouching
-    public float jumpVelocity = 0.00001f;
+    public float jumpVelocity = 25f;
+    private float slopeDistance = 1f;
 
     private Rigidbody2D my_rigidbody;
-    private bool my_grounded;
-    private bool my_ceilinged;
     private BoxCollider2D boxCollider;
     private CircleCollider2D circleCollider;
+
+    private bool isGrounded;
+    private bool isOnSlope = false;
+    private bool isMoveable = true;
+
+    //sloep check variables
+    private Vector2 slopeNormalPrep;
+    private float slopeDownAngle;
+    private float slopeSideAngle;
+    private float slopeDownAngleOld;
+    [SerializeField] private float maxSlopeAngle;
+  
+
     [SerializeField] private LayerMask platformLayerMask;
 
     private void Awake()
@@ -32,9 +44,9 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        my_grounded = isGrounded();
-        my_ceilinged = isCeilinged();
+        isGrounded = GroundCheck();
 
+        SlopeCheck();
         Jump();
     }
 
@@ -42,7 +54,7 @@ public class PlayerController : MonoBehaviour
     {
         //if player isn't touched ceiling and still on the ground
 
-        if (my_grounded)
+        if (isGrounded || isOnSlope)
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
@@ -57,22 +69,20 @@ public class PlayerController : MonoBehaviour
     private void Move()
     {
         //dash
-        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.LeftShift))
-            transform.position += new Vector3(1, 0) * dashSpeed * Time.deltaTime;
-        else if (Input.GetKey(KeyCode.A) && Input.GetKeyDown(KeyCode.LeftShift))
-            transform.position -= new Vector3(1, 0) * dashSpeed * Time.deltaTime;
-        //walk
-        else if (Input.GetKey(KeyCode.D))
-            transform.position += new Vector3(1, 0) * walkSpeed * Time.deltaTime;
-        else if (Input.GetKey(KeyCode.A))
-            transform.position -= new Vector3(1, 0) * walkSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.LeftShift) && isMoveable)
+            my_rigidbody.velocity = new Vector2(1 * dashSpeed, my_rigidbody.velocity.y);
+        else if (Input.GetKey(KeyCode.D) && isMoveable)
+            my_rigidbody.velocity = new Vector2(1 * walkSpeed, my_rigidbody.velocity.y);
+        else if (Input.GetKey(KeyCode.A) && isMoveable)
+            my_rigidbody.velocity = new Vector2(-1 * walkSpeed, my_rigidbody.velocity.y);
+
         else
         {
             my_rigidbody.velocity = new Vector3(0, my_rigidbody.velocity.y);
         }
     }
 
-    private bool isGrounded()
+    private bool GroundCheck()
     {
         float extraHeight = 0.1f;
         RaycastHit2D groundHit  = Physics2D.Raycast(circleCollider.bounds.center, Vector2.down, circleCollider.bounds.extents.y + extraHeight, platformLayerMask);
@@ -80,25 +90,72 @@ public class PlayerController : MonoBehaviour
 
         if (groundHit.collider != null)
             rayColor = Color.green;
-        Debug.DrawRay(circleCollider.bounds.center, Vector2.down * (circleCollider.bounds.extents.y + extraHeight));
+        //Debug.DrawRay(circleCollider.bounds.center, Vector2.down * (circleCollider.bounds.extents.y + extraHeight), rayColor);
         if (groundHit.collider != null)
             Debug.Log("Hit ground");
 
         return groundHit.collider != null;
     }
 
-    private bool isCeilinged()
+    private void SlopeCheck()
     {
-        float extraHeight = 0.1f;
-        RaycastHit2D ceilingHit = Physics2D.Raycast(boxCollider.bounds.center, Vector2.up, boxCollider.bounds.extents.y + extraHeight, platformLayerMask);
-        Color rayColor = Color.red;
+        //Vector2 checkPos = circleCollider.bounds.center - new Vector3(0.0f, circleCollider.bounds.size.y / 2 );
+        Vector2 checkPos = transform.position - new Vector3(0.0f, circleCollider.bounds.size.y / 2);
+        Debug.DrawLine(circleCollider.bounds.center, checkPos, Color.black); 
 
-        if (ceilingHit.collider != null)
-            rayColor = Color.green;
-        Debug.DrawRay(boxCollider.bounds.center, Vector2.up * (boxCollider.bounds.extents.y + extraHeight));
+        SlopeHorizontalCheck(checkPos);
+        SlopeVerticalCheck(checkPos);
 
-        return ceilingHit.collider != null;
     }
 
-  
+    private void SlopeHorizontalCheck(Vector2 checkPos)
+    {
+        RaycastHit2D slopeHitFront = Physics2D.Raycast(checkPos, transform.right, slopeDistance, platformLayerMask);
+        RaycastHit2D slopeHitBack = Physics2D.Raycast(checkPos, -transform.right, slopeDistance, platformLayerMask);
+
+        if (slopeHitBack)
+        {
+            isOnSlope = true;
+            slopeSideAngle = Vector2.Angle(slopeHitBack.normal, Vector2.up);
+        }
+        else if (slopeHitFront)
+        {
+            isOnSlope = true;
+            slopeSideAngle = Vector2.Angle(slopeHitFront.normal, Vector2.up);
+        }
+        else
+        {
+            slopeSideAngle = 0.0f;
+            isOnSlope = false;
+        }
+    }
+
+    private void SlopeVerticalCheck(Vector2 checkPos)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(checkPos, Vector2.down, slopeDistance, platformLayerMask);
+
+        if (hit)
+        {
+            slopeNormalPrep = Vector2.Perpendicular(hit.normal);
+            slopeDownAngle = Vector2.Angle(hit.normal, Vector2.up);
+           
+            if(slopeDownAngle != slopeDownAngleOld)
+            {
+                isOnSlope = true;
+            }
+
+
+            slopeDownAngleOld = slopeDownAngle;
+
+            Debug.DrawRay(hit.point, slopeNormalPrep, Color.red);
+            Debug.DrawRay(hit.point, hit.normal, Color.blue);
+            Debug.Log("slopeDownAngle");
+            Debug.Log(slopeDownAngle);
+        }
+
+        if (slopeDownAngle > maxSlopeAngle || slopeSideAngle > maxSlopeAngle)
+            isMoveable = false;
+        else
+            isMoveable = true;
+    }
 }
